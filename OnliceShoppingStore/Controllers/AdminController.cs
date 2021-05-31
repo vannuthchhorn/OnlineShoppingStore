d0ee166a865309e211428d180fcb5407069ef85e
vannuth chhorn <vannuthchhorn@gmail.com>
2021-05-31 14:52:31 +0700

    Add Admin layout, Dashboard and list category list

---
+++ OnliceShoppingStore/Controllers/AdminController.cs
@@ -0,0 +1,26 @@
+﻿using OnliceShoppingStore.DAL;
+using OnliceShoppingStore.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OnliceShoppingStore.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET: Admin
+        public GenericUnitOfWork _unitOfWork = new GenericUnitOfWork();
+        public ActionResult Dashboard()
+        {
+            return View();
+        }
+
+        public ActionResult Categories()
+        {
+            List<Tbl_Category> allCateogries = _unitOfWork.GetRepositoryInstance<Tbl_Category>().GetAllRecordsIQueryable().Where(i => i.IsDelete == false).ToList();
+            return View(allCateogries);
+        }
+    }
+}
