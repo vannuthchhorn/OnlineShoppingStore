3b80e1f4232bbe3c1ccf7153511b0dc4c0f16135
vannuth chhorn <vannuthchhorn@gmail.com>
2021-06-02 15:27:00 +0700

    checkout and checkoutDetails with count product order

--- OnliceShoppingStore/Controllers/HomeController.cs
+++ OnliceShoppingStore/Controllers/HomeController.cs
@@ -17,7 +17,7 @@ namespace OnliceShoppingStore.Controllers
             return View(model.CreatedModel(search,4, page));
         }
 
-        public ActionResult AddToCart(int productId)
+        public ActionResult AddToCart(int productId, string url)
         {
             if(Session["cart"] == null)
             {
@@ -33,21 +33,80 @@ namespace OnliceShoppingStore.Controllers
             else
             {
                 List<Item> cart = (List<Item>)Session["cart"];
+                var count = cart.Count();
                 var product = ctx.Tbl_Products.Find(productId);
-                cart.Add(new Item()
+                for (int i = 0; i < count; i++)
                 {
-                    Product = product,
-                    Quantity = 1
-                });
+                    if (cart[i].Product.ProductId == productId)
+                    {
+                        int prevQty = cart[i].Quantity;
+                        cart.Remove(cart[i]);
+                        cart.Add(new Item()
+                        {
+                            Product = product,
+                            Quantity = prevQty + 1
+                        });
+                        break;
+                    }
+                    else
+                    {
+                        var prd = cart.Where(x => x.Product.ProductId == productId).SingleOrDefault();
+                        if (prd == null)
+                        {
+                            cart.Add(new Item()
+                            {
+                                Product = product,
+                                Quantity = 1
+                            });
+                        }
+                    }
+                }
                 Session["cart"] = cart;
             }
-            return Redirect("Index");
+            return Redirect(url);
+        }
+
+        public ActionResult Checkout()
+        {
+            return View();
+        }
+
+        public ActionResult CheckoutDetails()
+        {
+            return View();
+        }
+
+        public ActionResult DecreaseQty(int productId)
+        {
+            if (Session["cart"] != null)
+            {
+                List<Item> cart = (List<Item>)Session["cart"];
+                var product = ctx.Tbl_Products.Find(productId);
+                foreach (var item in cart)
+                {
+                    if (item.Product.ProductId == productId)
+                    {
+                        int prevQty = item.Quantity;
+                        if (prevQty > 0)
+                        {
+                            cart.Remove(item);
+                            cart.Add(new Item()
+                            {
+                                Product = product,
+                                Quantity = prevQty - 1
+                            });
+                        }
+                        break;
+                    }
+                }
+                Session["cart"] = cart;
+            }
+            return Redirect("Checkout");
         }
 
         public ActionResult RemoveFromCart(int productId)
         {
             List<Item> cart = (List<Item>)Session["cart"];
-            //var product = ctx.Tbl_Products.Find(productId);
             foreach(var item in cart)
             {
                 if (item.Product.ProductId == productId)
